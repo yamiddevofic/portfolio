ba1e3f2b6e864965ed2fd4df5ebb24f4541f3537
yamid-dev <yhrodriguez1@hotmail.com>
2024-05-06 21:16:04 -0500

    señalización de partes

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -24,15 +24,15 @@ function CollapsibleExample() {
       if (!lightMode) {
         item.style.background = '#111';
         item.style.color = '#fff';
-        item.style.transition="background-color 1s";
+        item.style.transition="background-color 1.5s";
       } else {
         item.style.background = 'white';
         item.style.color = '#4d4d4d';
-        item.style.transition="background-color 1s";
+        item.style.transition="background-color 1.5s";
       }
       }
   });
-    root.style.background = !lightMode ? '#04122E' : timeOfDay();
+    root.style.background = !lightMode ? '#010101' : timeOfDay();
     root.style.transition="background-color 1s";
     container.style.transition="background-color 1s";
   };
@@ -44,7 +44,31 @@ function CollapsibleExample() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#sobreMi">Sobre Mi</Nav.Link>
+          <Nav.Link href="#sobreMi" onClick={() => {
+            let cont = 0;
+            const sobreMi = document.querySelectorAll('.items.sobreMi');
+            const sobreMiArray = Array.from(sobreMi).reverse(); // Convertir NodeList a un array y luego invertirlo
+
+            sobreMiArray.forEach(sobreMi => {
+              const intervalo = setInterval(() => {
+                cont++;
+                console.log(cont);
+                if (cont > 1) {
+                  sobreMi.style.background = "#ffe100";
+                  sobreMi.style.color = "#202020";
+                  sobreMi.style.transition = "background-color 1s";
+                }
+                if (cont > 3) {
+                  sobreMi.style.background = "white";
+                  sobreMi.style.color = "#202020";
+                  sobreMi.style.transition = "background-color 1s";
+                  clearInterval(intervalo);
+                  console.log("¡Intervalo cerrado!");
+                }
+              }, 1000);
+            });
+          }}>Sobre Mi</Nav.Link>
+
             <Nav.Link href="#datos" onClick={()=>{
               const datos = document.getElementById('datos');
               var cont = 0
@@ -55,18 +79,40 @@ function CollapsibleExample() {
                   if (cont>1){
                     datos.style.background = "#ffe100";
                     datos.style.color = "#202020";
-                    datos.style.transition="background-color 2s";
+                    datos.style.transition="background-color 1s";
                   }
                   if (cont>3){
                     datos.style.background = "white";
                     datos.style.color= "#202020";
-                    datos.style.transition="background-color 2s";
+                    datos.style.transition="background-color 1s";
                     clearInterval(intervalo);
                     console.log("¡Intervalo cerrado!")
                   }
                 },500
               )
             }}>Datos Personales</Nav.Link>
+            <Nav.Link href="#proyectos" onClick={()=>{
+              const proyectos = document.getElementById('proyectos');
+              var cont = 0
+              const intervalo = setInterval(
+                () => {
+                  cont ++;
+                  console.log(cont);
+                  if (cont>1){
+                    proyectos.style.background = "#ffe100";
+                    proyectos.style.color = "#202020";
+                    proyectos.style.transition="background-color 1s";
+                  }
+                  if (cont>3){
+                    proyectos.style.background = "white";
+                    proyectos.style.color= "#202020";
+                    proyectos.style.transition="background-color 1s";
+                    clearInterval(intervalo);
+                    console.log("¡Intervalo cerrado!")
+                  }
+                },500
+              )
+            }}>Proyectos</Nav.Link>
             <NavDropdown title="Hoja de vida" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -80,10 +126,11 @@ function CollapsibleExample() {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              <p style={{display:'inline-block'}}>Modo claro/oscuro </p>
-              <LightModeRoundedIcon sx={{marginLeft: '5px',display:'inline-block'}} onClick={cambiarModo} />
+              <div style={{display:'flex'}}>
+                <div style={{display:'inline-block'}}><p>Modo claro/oscuro </p></div>
+                <div style={{display:'inline-block'}} className="icon"><LightModeRoundedIcon onClick={cambiarModo} /></div>
+              </div>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
