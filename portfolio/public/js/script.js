f29a7da59a51c2527cc1abc0480dabb25863c666
yamid-dev <yhrodriguez1@hotmail.com>
2024-08-29 10:30:01 -0500

    Cambio colores fondo mañana

--- portfolio/public/js/script.js
+++ portfolio/public/js/script.js
@@ -8,7 +8,7 @@ function timeOfDay(){
     }
     //mañana
     else if(hora>4 && hora<12){
-        color="linear-gradient(to bottom, #FFD700, #FF8C00, #00BFFF, #1E90FF)";
+        color="linear-gradient(to bottom, #00BFFF, #1E90FF, #B0E0E6, #87CEEB )";
     }
     //tarde
     if (hora>11 && hora<=17){
