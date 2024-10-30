6d774de7aa52f6460dcffdf98b2589cef39264f3
Yamid Rodriguez <157556215+thecodeofyamid@users.noreply.github.com>
2024-10-30 01:23:01 +0000

    problema de despliegue solucionado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server', // Importante para que pueda manejar APIs del lado del servidor
   adapter: vercel(),
+  integrations: [
+    react(), // Añadir integración de React
+    tailwind({ config: './tailwind.config.cjs' }) // Añadir integración de Tailwind con el archivo de configuración específico
+  ],
 });
