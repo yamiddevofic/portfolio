a576de0e5d205a3b9bd5c601c022ca14872ee14b
pedromogollon0107 <pedromogollon0107@gmail.com>
2024-10-30 02:51:12 +0000

    Configurando serverless

--- api/database.js
+++ api/database.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 // Crear un pool de conexiones a MySQL
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
